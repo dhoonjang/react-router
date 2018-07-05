9e4d6bdabd6bada741e7bd385a70bbb2405be6fe
dhoonjang <dhoonjang@gmail.com>
2018-07-06 00:59:18 +0900

    기본 설정

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './client/Root';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
